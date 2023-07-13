b2e3795f92ac7916211b81b62a045ea9f9202c20
@hexlet/cli <support@hexlet.io>
2023-07-13 15:56:56 +0300

    submit java-collections-ru/generics

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -9,21 +9,19 @@ import java.util.Map.Entry;
 public class App {
     public static List<Map<String, String>> findWhere(List<Map<String, String>> books, Map<String, String> where) {
         List<Map<String, String>> result = new ArrayList<>();
-        for (Map<String, String> where : books) {
-            boolean find = true;
-
-            for (Entry<String, String> entry : where.entrySet()) {
-                String whereValue = where.getOrDefault(entry.getKey(), "");
-                if (!whereValue.equals(entry.getValue())) {
-                    find = false;
+        int length = where.size();
+        for (Map<String, String> book: books) {
+            int counter = 0;
+            for (Map.Entry<String, String> entryWhere: where.entrySet()) {
+                if (book.containsValue(entryWhere.getValue())) {
+                    counter++;
                 }
             }
-
-            if (find) {
-                result.add(where);
+            if (length == counter) {
+                result.add(book);
             }
         }
-
+        System.out.println(result);
         return result;
     }
 }
