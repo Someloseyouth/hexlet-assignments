2791fad45617f2fbccd77986c1381ddd82f8c663
@hexlet/cli <support@hexlet.io>
2023-07-16 21:28:52 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -1,29 +1,31 @@
 package exercise;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.List;
 import java.util.Arrays;
 import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
 class AppTest {
 
     @Test
-    void testTake() {
+    void testTake1() {
         // BEGIN
-        @Test
-        void testTake1() {
-            List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-            List<Integer> result = App.take(list, 2);
-            assertThat(result).isEqualTo(Arrays.asList(1, 2));
-        }
 
-        @Test
-        void testTake2() {
-            List<Integer> list = new ArrayList<>(Arrays.asList(7, 3, 10));
-            List<Integer> result = App.take(list, 8);
-            assertThat(result).isEqualTo(Arrays.asList(7, 3, 10));
-        }
-        // END
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        List<Integer> result = App.take(list, 2);
+        assertThat(result).isEqualTo(Arrays.asList(1, 2));
     }
+
+    @Test
+    void testTake2() {
+        List<Integer> list = new ArrayList<>(Arrays.asList(7, 3, 10));
+        List<Integer> result = App.take(list, 8);
+        assertThat(result).isEqualTo(Arrays.asList(7, 3, 10));
+    }
+    // END
 }
+
