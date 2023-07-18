8c552c852d6023b6eef892cc84f469bf764ab51a
@hexlet/cli <support@hexlet.io>
2023-07-18 19:40:58 +0300

    submit java-collections-ru/advanced-streams

---
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -0,0 +1,28 @@
+package exercise;
+
+import java.util.stream.Collectors;
+import java.util.*;
+
+// BEGIN
+class App{
+    public static String getForwardedVariables(String config){
+        String[] arr = content.split("\n");
+        List<String> ans = Arrays.stream(arr)
+                .filter(x -> x.startsWith("environment="))
+                .filter((x -> x.contains("X_FORWARDED_")))
+                .map(x -> x.replace("environment=", "")).toList();
+        String temp = ans.toString()
+                .replace("[\"", "")
+                .replace("\"]", "");
+        arr = temp.split("=");
+        Map<String, String> m = new LinkedHashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].contains("X_FORWARDED_")) {
+                arr[i+1] = arr[i+1].replace("\"", "");
+                m.put(arr[i].substring(arr[i].lastIndexOf("_") + 1), arr[i+1].substring(0, arr[i+1].indexOf(",")));
+            }
+        }
+        return m.toString().replace(" ", "").replace("{", "").replace("}", "");
+    }
+}
+//END
