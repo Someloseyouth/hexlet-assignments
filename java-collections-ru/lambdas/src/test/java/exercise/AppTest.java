be1fede6e09e321ca2b5d3828cf59afe327b08a1
@hexlet/cli <support@hexlet.io>
2023-07-18 18:28:20 +0300

    submit java-collections-ru/lambdas

---
+++ java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
@@ -0,0 +1,29 @@
+package exercise;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+// BEGIN
+class AppTest {
+    @Test
+    void testApp() {
+        String[][] image = {
+                {"*", "*", "*", "*"},
+                {"*", " ", " ", "*"},
+                {"*", " ", " ", "*"},
+                {"*", "*", "*", "*"},
+        };
+        String[][] enlargedImage = App.enlargeArrayImage(image);
+        assertThat(Arrays.deepToString(enlargedImage)).isEqualTo("[" +
+                "[*, *, *, *, *, *, *, *], " +
+                "[*, *, *, *, *, *, *, *], " +
+                "[*, *,  ,  ,  ,  , *, *], " +
+                "[*, *,  ,  ,  ,  , *, *], " +
+                "[*, *,  ,  ,  ,  , *, *], " +
+                "[*, *,  ,  ,  ,  , *, *], " +
+                "[*, *, *, *, *, *, *, *], " +
+                "[*, *, *, *, *, *, *, *]"   +
+                "]");
+    }
+}
+// END
