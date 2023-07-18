aac34cd607ce43155c14da8fcf19ec7ec34006d9
@hexlet/cli <support@hexlet.io>
2023-07-18 19:43:06 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -1,11 +1,11 @@
 package exercise;
 
 import java.util.stream.Collectors;
-import java.util.*;
+import java.util.Arrays;
 
 // BEGIN
-class App{
-    public static String getForwardedVariables(String config){
+class App {
+    public static String getForwardedVariables(String config) {
         String[] arr = content.split("\n");
         List<String> ans = Arrays.stream(arr)
                 .filter(x -> x.startsWith("environment="))
@@ -18,8 +18,8 @@ class App{
         Map<String, String> m = new LinkedHashMap<>();
         for (int i = 0; i < arr.length; i++) {
             if (arr[i].contains("X_FORWARDED_")) {
-                arr[i+1] = arr[i+1].replace("\"", "");
-                m.put(arr[i].substring(arr[i].lastIndexOf("_") + 1), arr[i+1].substring(0, arr[i+1].indexOf(",")));
+                arr[i + 1] = arr[i + 1].replace("\"", "");
+                m.put(arr[i].substring(arr[i].lastIndexOf("_") + 1), arr[i + 1].substring(0, arr[i + 1].indexOf(",")));
             }
         }
         return m.toString().replace(" ", "").replace("{", "").replace("}", "");
