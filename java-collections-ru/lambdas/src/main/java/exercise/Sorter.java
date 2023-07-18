bb3dd34d0cb665bd5cd5c1417b17017e3b6d58ae
@hexlet/cli <support@hexlet.io>
2023-07-18 18:31:00 +0300

    submit java-collections-ru/lambdas

--- java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
+++ java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 import java.util.stream.Collectors;
 
 // BEGIN
-// public class Sorter {
+public class Sorter {
     public static List<String> takeOldestMans(List<Map<String, String>> users) {
         return users.stream()
                 .filter(sex -> sex.containsValue("male"))
